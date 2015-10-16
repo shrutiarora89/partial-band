ce2dc9746c470f388dd6d6d0dbbfac97ff8425af
Shruti Arora <shruti.arora89@icloud.com>
2015-10-15 18:23:56 -0700

    Lab-3-submission

--- app/script.js
+++ app/script.js
@@ -1,39 +1,65 @@
 'use strict';
 
-$(function() {
-
-  $("button").click(function() {
-    $.get('adjective', function(response) {
+//get request to send the adjective to the server
+  $("#getAdjectiveButton").click(function() {
+    $.get('http://localhost:3003/adjective', function(response) {
       var adjective = response.word;
       $("#adjective").text(adjective);
     });
+  });
 
-    $.get('verb', function(response) {
+//get request to send the verb to the server
+  $("#getVerbButton").click(function() {
+    $.get('http://localhost:3003/verb', function(response) {
       var verb = response.word;
       $("#verb").text(verb);
     });
+  });
 
-    $.get('noun', function(response) {
+//get request to send the noun to the server
+  $("#getNounButton").click(function() {
+    $.get('http://localhost:3003/noun', function(response) {
       var noun = response.word;
       $("#noun").text(noun);
     });
-
   });
 
+
+// on click event on submit button
   $("#submitWords").on("submit", function(e) {
     e.preventDefault();
 
+// storing values of adjective, noun and verb
     var adjective = $("input[name=adjective]").val();
-    var adjPost;
+    var noun = $("input[name=noun]").val();
+    var verb = $("input[name=verb]").val();
 
+
+//if its the adjective then respond with adjective word
     if (adjective) {
-      adjPost = {word: adjective};
-      $.post("adjective", adjPost, function(response) {
-        var adjectiveRes = response.msg;
+      var adjPost = {word: adjective};
+      $.post("http://localhost:3003/adjective", adjPost, function(response) {
+        var adjectiveRes = response;
         $("#adjectiveRes").text(adjectiveRes);
       });
     }
 
-  });
+//if its the noun then respond with noun word
+    if (noun) {
+      var nounPost = {word: noun};
+      $.post("http://localhost:3003/noun", nounPost, function(response) {
+        var nounRes = response;
+        $("#nounRes").text(nounRes);
+      });
+    }
 
-});
+//if its the verb then respond with verb word
+    if (verb) {
+      var verbPost = {word: verb};
+      $.post("http://localhost:3003/verb", verbPost, function(response) {
+        var verbRes = response;
+        $("#verbRes").text(verbRes);
+      });
+    }
+
+  });
--- server.js
+++ server.js
@@ -1,84 +1,99 @@
 'use strict';
 
+//other file u have created.
 var express = require('express');
 var app = express();
+
 var bodyparser = require('body-parser');
-var port = process.env.PORT || 3000;
-var Adjective = require('./lib/adjective.js');
-var getRandomWord = require('./lib/getRandomWord.js');
-
-var adjective = new Adjective();
-
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
   extended: true
 }));
 
+//storing the funcionality into variables
+var getRandomWord = require('./lib/getRandomWord.js');
+var Adjective = require('./lib/adjective.js');
+var Noun = require('./lib/noun.js');
+var Verb =require('./lib/verb.js');
+
+//storing the server port in a variable
+var port = 3003;
+
+//telling the server that we have directory of app
 app.use(express.static(__dirname + '/app/'));
 
-var Verb = function() {
-  this.sleeping = true;
-  this.skiing = true;
-  this.flopped = true;
-  this.waiting = true;
-  this.plotting = true;
-  this.burping = true;
-  this.sneezing = true;
-  this.flying = true;
-  this.snoozing = true;
-  this.dancing = true;
-};
-var verb = new Verb();
 
-var Noun = function() {
-  this.ninjas = true;
-  this.cowboys = true;
-  this.bears = true;
-  this.beats = true;
-  this.alligators = true;
-  this.hamsters = true;
-  this.otters = true;
-  this.helicopters = true;
-  this.unicorns = true;
-  this.rhymes = true;
-};
-var noun = new Noun();
-
-function postWord (word, wordObject) {
-  if (wordObject.hasOwnProperty(word)) {
-    return {msg: 'We already have your awesome word, ' + word + ', in our list.'};
-  }
-
-  wordObject[word] = true;
-  console.dir(wordObject);
-  return {msg: 'Thanks for submitting ' + word + '!'};
-};
-
-app.get('/adjective', function(req, res) {
-  res.json(getRandomWord(adjective));
-});
-
-app.get('/verb', function(req, res) {
-  res.json(getRandomWord(verb));
-});
-
-app.get('/noun', function(req, res) {
-  res.json(getRandomWord(noun));
-});
-
-app.post('/adjective', function(req, res) {
-  var word = postWord(req.body.word, adjective);
-  res.json(word);
-});
-
-app.get('/', function(req, res) {
-  res.sendFile('index.html');
-});
-
+//this will start the server
 app.listen(port, function() {
   console.log('server starting. available at http://localhost:' + port);
 });
 
-// app.get('/', function(req, res) {
-//   res.send('hello, universe');
-// });
+//send the index file to the server
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
+});
+
+
+var postWord = function(word,object) {
+  object[word] = true;
+}
+
+/**** creating ADJECTIVES instances****/
+var adjectives = new Adjective();
+
+//retrive from the server adjective words
+app.get('/adjective', function(req, res) {
+  res.json(getRandomWord(adjectives));
+});
+
+//if the word is adjective then post adjective
+app.post('/adjective', function(req,res) {
+  var word = req.body.word;
+  if (word) {
+    postWord(word,adjectives);
+    console.log(adjectives);
+    res.send("Added " + word + " to list of adjectives");
+  }
+
+});
+
+/********************/
+
+/**** creating NOUNS instances ****/
+var nouns = new Noun();
+
+//retrive from the server noun words
+app.get('/noun', function(req, res) {
+  res.json(getRandomWord(nouns));
+});
+
+//if the word is noun then post noun
+app.post('/noun', function(req,res) {
+  var word = req.body.word;
+  if (word) {
+    postWord(word,nouns);
+    res.send("Added " + word + " to list of nouns");
+  }
+});
+
+/********************/
+
+/**** creating VERBS instances****/
+var verbs = new Adjective();
+
+//retrive from the server verb words
+app.get('/verb', function(req, res) {
+  res.json(getRandomWord(verbs));
+});
+
+//if the word is verb then post verb
+app.post('/verb', function(req,res) {
+  var word = req.body.word;
+  if (word) {
+    postWord(word,verbs);
+    console.log(adjectives);
+    res.send("Added " + word + " to list of verbs");
+  }
+});
+/********************/
+
